f312f680f9bcc88359c554c87441e33ff82e7b9f
Dag Liodden <dag@tapad.com>
2013-08-16 14:50:09 -0400

    Make future pool configurable. Clean response functions.

--- unfinagled-core/src/main/scala/com/novus/unfinagled/UnfilteredService.scala
+++ unfinagled-core/src/main/scala/com/novus/unfinagled/UnfilteredService.scala
@@ -1,13 +1,12 @@
 package com.novus.unfinagled
 
 import com.twitter.finagle.Service
-import com.twitter.util.Future
+import com.twitter.util.{ FuturePool, Future }
 import org.jboss.netty.handler.codec.http._
 import org.jboss.netty.handler.codec.http.HttpVersion._
 import org.jboss.netty.handler.codec.http.HttpResponseStatus._
 import unfiltered.response.{ Connection, ContentLength, NotFound, Responder, HttpResponse => UHttpResponse }
 import unfiltered.netty.ResponseBinding
-import unfiltered.netty.cycle.Plan.Intent
 
 /** Functions for constructing finagle Services from unfiltered types.
  */
@@ -17,30 +16,42 @@ object UnfilteredService {
   val nettyResponse: HttpResponseStatus => HttpResponse =
     new DefaultHttpResponse(HTTP_1_1, _)
 
-  def apply(intent: Intent) =
+  /** Creates a Finagle wrapper service around a Unfiltered synchronous / cycle intent.
+   *
+   *  @param intent the intent to wrap
+   *  @param futurePool the pool on which the intent will be applied.
+   *                   The default is the an immediate pool that assumes the intent is non-blocking and thus
+   *                   can be executed on the current I/O worker thread.
+   *  @return the Finagle service wrapper
+   */
+  def apply(intent: unfiltered.netty.cycle.Plan.Intent, futurePool: FuturePool = FuturePool.immediatePool) =
     new Service[HttpRequest, HttpResponse] {
       def apply(request: HttpRequest): Future[HttpResponse] =
         request match {
           case uf: RequestAdapter =>
-            Future {
+            futurePool {
               val rf = intent.lift(uf.binding).getOrElse(NotFound)
-              val nres = new ResponseBinding(nettyResponse(OK))
-              val keepAlive = HttpHeaders.isKeepAlive(request)
-              val closer = new Responder[HttpResponse] {
-                def respond(res: UHttpResponse[HttpResponse]) {
-                  res.outputStream.close()
-                  (if (keepAlive)
-                    Connection("Keep-Alive") ~>
-                    ContentLength(
-                      res.underlying.getContent().readableBytes().toString)
-                  else unfiltered.response.Connection("close"))(res)
-                }
-              }
-              (rf ~> closer)(nres).underlying
+              val nres = new ResponseBinding(response(request, OK))
+              (rf ~> keepAlive(request))(nres).underlying
             }
           case _ =>
             // TODO should probably log this as it indicates a problem with handler ordering in the pipeline.
-            Future(nettyResponse(INTERNAL_SERVER_ERROR))
+            Future(response(request, INTERNAL_SERVER_ERROR))
         }
     }
+
+  private final def response(request: HttpRequest, status: HttpResponseStatus): HttpResponse =
+    new DefaultHttpResponse(request.getProtocolVersion, status)
+
+  private final def keepAlive(request: HttpRequest) = new Responder[HttpResponse] {
+    def respond(res: UHttpResponse[HttpResponse]) {
+      res.outputStream.close()
+      (if (HttpHeaders.isKeepAlive(request))
+        Connection("Keep-Alive") ~>
+        ContentLength(
+          res.underlying.getContent().readableBytes().toString)
+      else unfiltered.response.Connection("close"))(res)
+    }
+  }
+
 }
