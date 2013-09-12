7b31fef34be54a2c7d1a97eac34ce65cb00ab498
Chris <clewis@novus.com>
2013-09-12 18:15:13 -0400

    remove unnecessary aliases

--- unfinagled-core/src/main/scala/com/novus/unfinagled/FuturePlan.scala
+++ unfinagled-core/src/main/scala/com/novus/unfinagled/FuturePlan.scala
@@ -3,9 +3,10 @@ package com.novus.unfinagled
 import unfiltered.request.HttpRequest
 import unfiltered.netty.ReceivedMessage
 import unfiltered.response.ResponseFunction
-import org.jboss.netty.handler.codec.http.{ HttpResponse => NHttpResponse }
+import org.jboss.netty.handler.codec.http.HttpResponse
+import com.twitter.finagle.http.service.RoutingService
 import scala.concurrent.Future
 
 object FuturePlan {
-  type Intent = PartialFunction[HttpRequest[ReceivedMessage], Future[ResponseFunction[NHttpResponse]]]
+  type Intent = PartialFunction[HttpRequest[ReceivedMessage], Future[ResponseFunction[HttpResponse]]]
 }
--- unfinagled-core/src/main/scala/com/novus/unfinagled/TwitterFuturePlan.scala
+++ unfinagled-core/src/main/scala/com/novus/unfinagled/TwitterFuturePlan.scala
@@ -3,8 +3,10 @@ package com.novus.unfinagled
 import unfiltered.request.HttpRequest
 import unfiltered.netty.ReceivedMessage
 import unfiltered.response.ResponseFunction
-import org.jboss.netty.handler.codec.http.{ HttpResponse => NHttpResponse }
+import org.jboss.netty.handler.codec.http.HttpResponse
+import com.twitter.finagle.http.service.RoutingService
+import com.twitter.util.Future
 
 object TwitterFuturePlan {
-  type Intent = PartialFunction[HttpRequest[ReceivedMessage], com.twitter.util.Future[ResponseFunction[NHttpResponse]]]
+  type Intent = PartialFunction[HttpRequest[ReceivedMessage], Future[ResponseFunction[HttpResponse]]]
 }
